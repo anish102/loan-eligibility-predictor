547142dd8464388ae233cde1ec87876cc33e76d2
anish102 <anishkarki695@gmail.com>
2024-12-13 21:49:51 +0545

    Modify register endpoint

--- app/routers/bank.py
+++ app/routers/bank.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, File, Form, HTTPException, UploadFile, status
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
 from pydantic import BaseModel
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 
 from app.auth import create_access_token, get_password_hash, verify_password
 from app.database import get_db
-from app.models import Bank
+from app.models import Bank, BankRegistrationDocument
 
 router = APIRouter()
 
@@ -23,19 +23,32 @@ class BankPass(BankBase):
 
 
 @router.post("/register")
-async def register(bank: BankPass, db: Session = Depends(get_db)):
-    existing_bank_id = db.query(Bank).filter(Bank.id == bank.id).first()
-    existing_bank_name = db.query(Bank).filter(Bank.name == bank.name).first()
+async def register(
+    id: str = Form(...),
+    name: str = Form(...),
+    password: str = Form(...),
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db),
+):
+    existing_bank_id = db.query(Bank).filter(Bank.id == id).first()
+    existing_bank_name = db.query(Bank).filter(Bank.name == name).first()
     if existing_bank_id or existing_bank_name:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Bank with the given id or name already exists.",
         )
-    hashed_password = get_password_hash(bank.password)
-    new_bank = Bank(id=bank.id, name=bank.name, hashed_password=hashed_password)
+    hashed_password = get_password_hash(password)
+    new_bank = Bank(id=id, name=name, hashed_password=hashed_password)
     db.add(new_bank)
     db.commit()
     db.refresh(new_bank)
+    file_content = await file.read()
+    new_document = BankRegistrationDocument(
+        filename=file.filename, file_content=file_content, bank_id=new_bank.id
+    )
+    db.add(new_document)
+    db.commit()
+    db.refresh(new_document)
     return {"message": "Bank registered successfully", "name": new_bank.name}
 
 
